18064812935a7273868cbc21016f8525bbbf7bad
midrees <midreeskhan7276@gmail.com>
2024-03-02 12:55:14 +0500

    update

--- signIn.js
+++ signIn.js
@@ -84,34 +84,33 @@ let signInWithGoogle = () => {
   
   
   }
-  const withFacebook=()=>{
-   
-signInWithPopup(auth, provider1)
-  .then((result) => {
-    // The signed-in user info.
-    const user = result.user;
+  const withFacebook = () => {
+    signInWithPopup(auth, provider1)
+        .then((result) => {
+            // The signed-in user info.
+            const user = result.user;
 
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-window.location.href="index.html"
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = FacebookAuthProvider.credentialFromError(error);
-    
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            const credential = FacebookAuthProvider.credentialFromResult(result);
+            const accessToken = credential.accessToken;
 
-    window.location.href="index.html"
-  });
+            // Redirect the user to index.html
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = FacebookAuthProvider.credentialFromError(error);
 
+            // Redirect the user to index.html even if an error occurs
+            window.location.href = "index.html";
+        });
+}
 
-
-  }
 
 const signINDirect=()=>{
   window.location.href="signUp."
